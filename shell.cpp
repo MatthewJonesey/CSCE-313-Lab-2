99f76cd242333db7fbe6bdc280a1fd184bd01db2
MatthewJonesey <matthew.jones6736@tamu.edu>
2025-09-09 19:44:25 +0000

    Second attempt

--- shell.cpp
+++ shell.cpp
@@ -2,6 +2,7 @@
 LE2: Introduction to Unnamed Pipes
 ****************/
 #include <unistd.h> // pipe, fork, dup2, execvp, close
+#include <iostream> // cout, endl
 using namespace std;
 
 int main () {
@@ -16,23 +17,24 @@ int main () {
     pipe(RW);
 
     // Create child to run first command
-    fork();
-    // In child, redirect output to write end of pipe
-    dup2(RW[1], STDOUT_FILENO);
-    // Close the read end of the pipe on the child side
-    close(RW[0]);
-    // In child, execute the command
-    execvp(cmd1[0], cmd1);
+    if (fork() == 0){
+        // In child, redirect output to write end of pipe
+        dup2(RW[1], STDOUT_FILENO);
+        // Close the read end of the pipe on the child side
+        close(RW[0]);
+        // In child, execute the command
+        execvp(cmd1[0], cmd1);
+    }
 
     // Create another child to run second command
-    fork();
-    // In child, redirect input to the read end of the pipe
-    dup2(RW[0], STDIN_FILENO);
-    // Close the write end of the pipe on the child side.
-    close(RW[1]);
-    // Execute the second command.
-    execvp(cmd2[0], cmd2);
-
+    if (fork() == 0){
+        // In child, redirect input to the read end of the pipe
+        dup2(RW[0], STDIN_FILENO);
+        // Close the write end of the pipe on the child side.
+        close(RW[1]);
+        // Execute the second command.
+        execvp(cmd2[0], cmd2);
+    }
     // Reset the input and output file descriptors of the parent.
 
 }
